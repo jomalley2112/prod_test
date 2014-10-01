1baf04b24e20e8d2a0b971ab6c989e9fddd2cdcb
John O'Malley <jom@nycap.rr.com>
2014-09-30 21:27:49 -0400

    temp comment out non-string columns in sql_searchable() call in schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ActiveRecord::Base
 
 	extend SqlSearchableSortable
 
-	sql_searchable :name, :birthday, :lunchtime, :apocalypse, :epoch, :christmas, :alarm_setting, :suppertime, :beer_oclock, :sleepytime, :party_time, :easter
+	sql_searchable :name #, :birthday, :lunchtime, :apocalypse, :epoch, :christmas, :alarm_setting, :suppertime, :beer_oclock, :sleepytime, :party_time, :easter
 
 	sql_sortable :name, :birthday, :lunchtime, :apocalypse, :epoch, :christmas, :alarm_setting, :suppertime, :beer_oclock, :sleepytime, :party_time, :easter
 end
